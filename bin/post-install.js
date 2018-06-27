966cb4cb982381594d110eddf9458bfacebb40f2
ZHAO Xudong <zxdong@gmail.com>
2018-06-27 09:50:24 +0800

    add test (#2)

---
+++ bin/post-install.js
@@ -0,0 +1,12 @@
+/**
+ * post install script
+ */
+const {cp} = require('shelljs')
+const {existsSync} = require('fs')
+const {resolve} = require('path')
+const prePushPath = resolve(__dirname, '../.git/hooks/pre-push')
+const prePushPathFrom = resolve(__dirname, 'pre-push')
+
+if (!existsSync(prePushPath)) {
+  cp(prePushPathFrom, prePushPath)
+}
