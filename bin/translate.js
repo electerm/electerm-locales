5ee978ba466daa69a8b6f5b3de071f77e51edc17
ZHAO Xudong <zxdong@gmail.com>
2018-12-19 15:47:46 +0800

    save

--- bin/translate.js
+++ bin/translate.js
@@ -3,8 +3,10 @@
  */
 
 const {exec} = require('child_process')
-const en = require('../locales/en_us')
 const _ = require('lodash')
+const template = require('fs').readFileSync(
+  'locales/en_us.js'
+).toString()
 
 const targets = {
   'ar': {
@@ -80,5 +82,13 @@ async function translate(tar, def) {
   let name = _.isString(def)
     ? def
     : def.name
+  let match = _.isString(def)
+    ? undefined
+    : def.match
+  let fileName = tar.toLowerCase().replace('-', '_') + '.js'
+  let langName = await trans({
+    tar,
+    txt: name
+  })
 
 }
